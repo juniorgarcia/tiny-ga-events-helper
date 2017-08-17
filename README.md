364baf884bf56dba89cb7ecfae7caceb1220fb5d
Júnior Garcia <itamarjr91@gmail.com>
2017-08-16 22:38:45 -0300

    Now keys' values for events data can be callbacks; Enhanced some structure; README.me updated.

--- README.md
+++ README.md
@@ -19,6 +19,14 @@ var events = [{
     eventValue: 'myValue'
 }];
 ```
+
+The values for the `events` keys can be callbacks. This is useful when the event data changes according to
+the element that triggers it. Inside those callbacks, `this` refers to the **individual**
+DOM element. By now, only four keys for the events are allowed, they are the same as Google Analytics uses:
+**eventCategory**, **eventAction**, **eventLabel** and **eventValue**.
+See [index.html](https://github.com/juniorgarcia/google-analytics-event-helper/blob/master/index.html#L30) to check how
+it works. Remember to check the HTML to understand this example.
+
 3. Create a `AnalyticsEventHelper` object passing the array of events:
 
 ```
@@ -32,3 +40,10 @@ That's it. Working :)
 You can add more events with the method `addEvent(event)`, or `addEvents(events)`. The only difference between them
 is that `addEvent(event)` receives an object, not an array, as `addEvents(events)` receives an array exactly like the
 one used to initialize the `AnalyticsEventHelper` object.
+
+## Simple serving the index.html for test.
+To serve the index.html, open your terminal and install the dependencies like so:
+
+```
+$ npm install; npm install -g gulp-cli; gulp serve;
+```
--- src/gaEvHelper.js
+++ src/gaEvHelper.js
@@ -3,6 +3,7 @@
 function AnalyticsEventHelper(events) {
     this.events = events;
     this._mandatoryKeys = ["domEvent", "el", "eventCategory", "eventAction"];
+    this._availableKeysForEvent = ["eventCategory", "eventAction", "eventLabel", "eventValue"];
 
     console.group("Received events: ");
     console.log(this.events);
@@ -19,9 +20,9 @@ AnalyticsEventHelper.prototype._isValidConfig = function() {
     if (this.events instanceof Array &&
         this.events.length > 0) {
         console.info('Configuration is Array and bigger than zero.');
-        for(var i = 0; i <= this.events.length - 1; i++) {
+        for(var i = 0; i < this.events.length; i++) {
             var currentEvent = this.events[i];
-            for(var j = 0; j <= this._mandatoryKeys.length - 1; j++) {
+            for(var j = 0; j < this._mandatoryKeys.length; j++) {
                 var currentMandatoryKey = this._mandatoryKeys[j];
                 console.info('Checking if configuration has mandatory key "' + currentMandatoryKey + '"');
                 if (
@@ -47,18 +48,57 @@ AnalyticsEventHelper.prototype.addEvents = function(events) {
     }
 };
 
+AnalyticsEventHelper.prototype._extractValuesFromEvent = function(event, el) {
+    var keys = Object.keys(event);
+    var values = {};
+    console.group('Extracting values from event:');
+    for(var i = 0; i < keys.length; i++) {
+        var key = keys[i];
+        if (this._availableKeysForEvent.indexOf(key) == -1) {
+            console.log('Invalid key for GA event:"' + key + '".');
+            continue;
+        }
+
+        var value = event[key];
+        if (typeof value === 'function') {
+            console.info('Return type of the event key "' + key + '" is a function.');
+            values[key] = value.apply(el);
+        } else {
+            console.info('Return type of the event key "' + key + '" is a literal "' + value + '".');
+            values[key] = value;
+        }
+    }
+    console.group("Values:");
+    console.log(values);
+    console.groupEnd();
+    console.groupEnd();
+    return values;
+};
+
 AnalyticsEventHelper.prototype.addEvent = function(event) {
+    var instance = this;
+    function extend (target, source) {
+        target = target || {};
+        for (var prop in source) {
+            if (typeof source[prop] === 'object') {
+                target[prop] = extend(target[prop], source[prop]);
+            } else {
+                target[prop] = source[prop];
+            }
+        }
+        return target;
+    }
+
     document.addEventListener('DOMContentLoaded', function() {
         var el = document.querySelectorAll(event.el);
         for(var i = 0; i < el.length; i++) {
             el[i].addEventListener(event.domEvent, function() {
-                ga('send', {
-                    hitType: 'event',
-                    eventCategory: event.eventCategory,
-                    eventAction: event.eventAction,
-                    eventLabel: event.eventLabel,
-                    eventValue: event.eventValue
-                });
+                ga('send',
+                    extend(
+                        {hitType: 'event'},
+                        instance._extractValuesFromEvent(event, this)
+                    )
+                );
             });
         }
     });
